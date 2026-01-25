a3fb265d68be9d931b7c356fca789799b86b9f7d
Dimitar Kupanov <dimitarkupanov@gmail.com>
2026-01-25 16:22:08 +0200

    lwIP options

--- include/lwipopts.h
+++ include/lwipopts.h
@@ -1,137 +1,123 @@
-#ifndef LWIPOPTS_H
-#define LWIPOPTS_H
+#pragma once
 
-/* ===============================================================
- * System
- * =============================================================== */
-#define NO_SYS                         0
-#define SYS_LIGHTWEIGHT_PROT           1
+/* ### Debuging ### */
 
-/* No errno */
-#define LWIP_PROVIDE_ERRNO             0
-#define LWIP_ERRNO_STDINCLUDE          0
-#define LWIP_SOCKET_SET_ERRNO          0
+/* === Assertion handling to disable in cc.h --> define LWIP_NOASSERT === */
 
-/* ===============================================================
- * Memory (CRITICAL)
- * =============================================================== */
-#define MEM_ALIGNMENT                  4
-#define MEM_SIZE                       (4 * 1024)   /* lwIP heap */
+/* === Statistics === */
 
-/* Use lwIP heap, not malloc */
-#define MEM_LIBC_MALLOC                0
-#define MEMP_MEM_MALLOC                0
+/* === Debug messages === */
 
-/* ===============================================================
- * Pbufs
- * =============================================================== */
-#define PBUF_POOL_SIZE                 4
-#define PBUF_POOL_BUFSIZE              512
+#define LWIP_DEBUG
 
-/* ===============================================================
- * Pools (VERY SMALL)
- * =============================================================== */
-#define MEMP_NUM_PBUF                  4
-#define MEMP_NUM_RAW_PCB               2
-#define MEMP_NUM_UDP_PCB               2
-#define MEMP_NUM_TCP_PCB               2
-#define MEMP_NUM_TCP_PCB_LISTEN        1
-#define MEMP_NUM_TCP_SEG               4
-#define MEMP_NUM_NETBUF                2
-#define MEMP_NUM_NETCONN               2
-#define MEMP_NUM_SYS_TIMEOUT           4
+#define DHCP_DEBUG LWIP_DBG_ON
+#define NETIF_DEBUG LWIP_DBG_ON
 
-/* ===============================================================
- * Network
- * =============================================================== */
-#define LWIP_NETIF_API                 1
-#define LWIP_NETIF_STATUS_CALLBACK     0
-#define LWIP_NETIF_LINK_CALLBACK       0
+/* === Performance === */
+#define LWIP_PERF 0
 
-#define LWIP_ARP                       1
-#define ARP_TABLE_SIZE                 4
-#define ARP_QUEUEING                   0
+/* ### Infrastructure ### */
 
-/* ===============================================================
- * IP
- * =============================================================== */
-#define LWIP_IPV4                      1
-#define LWIP_IPV6                      0
+/* === OS === */
+#define NO_SYS	1
 
-#define IP_REASSEMBLY                  0
-#define IP_FRAG                        0
-#define IP_FORWARD                     0
+/* === Timers === */
+#define LWIP_TIMERS 1
+#define LWIP_TIMERS_CUSTOM 0
 
-/* ===============================================================
- * ICMP
- * =============================================================== */
-#define LWIP_ICMP                      1
+/* === Memcpy look at doxygen doc === */
 
-/* ===============================================================
- * UDP / TCP
- * =============================================================== */
-#define LWIP_UDP                       1
-#define LWIP_TCP                       1
+/* === Core locking and MPU === */
+#define LWIP_MPU_COMPATIBLE 0
+#define LWIP_TCPIP_CORE_LOCKING 0
+#define SYS_LIGHTWEIGHT_PROT 0
 
-#define TCP_MSS                        536
-#define TCP_SND_BUF                    (2 * TCP_MSS)
-#define TCP_WND                        (2 * TCP_MSS)
-#define TCP_SND_QUEUELEN               4
+/* === Heap and memory pools === */
+#define MEMP_MEM_INIT 1
+#define MEM_ALIGNMENT 4
+#define MEM_SIZE (4 * 1024)
+#define MEMP_OVERFLOW_CHECK 2
+#define MEMP_SANITY_CHECK 1 //TODO: maybe in debug
+#define MEM_OVERFLOW_CHECK 2
+#define MEM_SANITY_CHECK 1 //TODO: maybe in debug
 
-#define TCP_LISTEN_BACKLOG             0
-#define LWIP_TCP_KEEPALIVE             0
+/* === Internal memory pools === */
+#define MEMP_NUM_PBUF 16
+#define MEMP_NUM_RAW_PCB 4
+#define MEMP_NUM_UDP_PCB 4
+#define MEMP_NUM_REASSDATA 5
+#define MEMP_NUM_FRAG_PBUF 15
+#define PBUF_POOL_SIZE 10
 
-/* ===============================================================
- * APIs (IMPORTANT)
- * =============================================================== */
-#define LWIP_SOCKET                    0   /* sockets are EXPENSIVE */
-#define LWIP_NETCONN                   1
-#define LWIP_RAW                       1
+/* === SNMP MIB2 callbacks === */
+#define LWIP_MIB2_CALLBACKS 0
 
-/* ===============================================================
- * DHCP / DNS (DISABLE)
- * =============================================================== */
-#define LWIP_DHCP                      0
-#define LWIP_DNS                       0
-#define LWIP_AUTOIP                   0
+/* === Multicast === */
+//#define LWIP_MULTICAST_TX_OPTIONS
 
-/* ===============================================================
- * Statistics & Debug (DISABLE)
- * =============================================================== */
-#define LWIP_STATS                     0
-#define LWIP_DEBUG                     0
-#define LWIP_NOASSERT                  1
+/* === Threading === */
 
-/* ===============================================================
- * Checksums
- * =============================================================== */
-#define CHECKSUM_GEN_IP                1
-#define CHECKSUM_GEN_UDP               1
-#define CHECKSUM_GEN_TCP               1
-#define CHECKSUM_CHECK_IP              1
-#define CHECKSUM_CHECK_UDP             1
-#define CHECKSUM_CHECK_TCP             1
+/* === Checksums === */
 
-/* ===============================================================
- * FreeRTOS tuning
- * =============================================================== */
-#define TCPIP_THREAD_STACKSIZE         512
-#define TCPIP_THREAD_PRIO              2
+/* === Hooks === */
 
-#define DEFAULT_THREAD_STACKSIZE       512
-#define DEFAULT_THREAD_PRIO            2
+/* ### Callback-style APIs ### */
 
-#define DEFAULT_ACCEPTMBOX_SIZE        2
-#define DEFAULT_TCP_RECVMBOX_SIZE      2
-#define DEFAULT_UDP_RECVMBOX_SIZE      2
+/* === RAW === */
+#define LWIP_RAW 1
 
-#define LWIP_DISABLE_TCP_SANITY_CHECKS 1
+/* === DNS === */
+#define LWIP_DNS 0
 
-#define USE_SLIPIF 0
+/* === UDP === */
+#define LWIP_UDP 1
+#define LWIP_UDPLITE 1
 
-#define sio_send
-#define sio_read
-#define sio_open
-#define sio_tryread
+/* === TCP === */
+#define LWIP_TCP 0
 
-#endif /* LWIPOPTS_H */
+/* ### Thread-safe APIs ### */
+
+/* === Netconn === */
+#define LWIP_NETCONN 0
+//LWIP_NETCONN_SEM_PER_THREAD TODO
+
+/* === Sockets === */
+#define LWIP_SOCKET 0
+
+/* ### IPv4 ### */
+#define LWIP_IPV4 1
+#define IP_REASS_MAX_PBUFS 10
+
+/* === ARP === */
+#define LWIP_ARP 1
+#define ARP_TABLE_SIZE 3
+
+/* === ICMP === */
+#define LWIP_ICMP 1
+
+/* === DHCP === */
+#define LWIP_DHCP 1
+
+/* === AUTOIP === */
+#define LWIP_AUTOIP 0
+
+/* === ACD === */
+
+/* === IGMP === */
+#define LWIP_IGMP 0
+
+/* ### PBUF ### */
+#define PBUF_POOL_BUFSIZE 1536
+
+/* ### NETIF ### */
+#define LWIP_SINGLE_NETIF 1
+#define LWIP_NETIF_HOSTNAME 1
+#define LWIP_NETIF_TX_SINGLE_PBUF 1
+
+/* === Loopback === */
+
+// STUPID SIO NEEDED BEACUSE NOT BUILD
+#define PPP_SUPPORT 0
+#define PPPOS_SUPPORT 0
+#define SLIPIF 0
